154e6fd02631efdda82465ae6e67e011b9ba1a63
ChaOscDelEch <julienhoussin.81@gmail.com>
2025-05-26 15:48:09 +0200

    Finalized Third Party Library

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ import 'leaflet/dist/leaflet.css'; // import leaflet css
 const WBS = [52.457131, 13.54007]; // WBS coordinates
 const map = Leaflet.map('map').setView(WBS, 13); // create a map object with a center and zoom level
 const markerIcon = Leaflet.icon({
-  iconUrl: '<https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png>',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   iconAnchor: [10, 20]
 }); // There was an issue with the default marker icon, so we create a new one
 Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
  
-Leaflet.tileLayer('<https://tile.openstreetmap.org/{z}/{x}/{y}.png>', {
+Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; <a href="<https://google.com>" target="_blank">Google</a>'
+  attribution: '&copy; <a href="https://google.com" target="_blank">Google</a>'
 }).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
 
 const myLocations = [
@@ -34,6 +34,11 @@ const myLocations = [
     name: 'Berlin Wall',
     location: [52.507541, 13.39032],
     description: 'The most famous wall in Berlin'
+  },
+  {
+    name: 'The place to be',
+    location : [52.519934022481245, 13.469321093076436],
+    description : 'Peace & Wonders'
   }
 ];
 
